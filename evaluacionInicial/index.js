d23060f6b2d62d2af1011390a65f83606ace94bf
Marco <marco.pertegal.prieto@gmail.com>
2023-09-25 21:20:59 +0200

    Erores en prueba inicial

--- evaluacionInicial/index.js
+++ evaluacionInicial/index.js
@@ -2,15 +2,19 @@ $(document).ready(function () {
 
     $(document).on('click', '#createURL', function () {
 
-        var url = $('#slug').val;
+        var url = $('#slug').val();
 
-        return url.replace("/", "-");
+        url.replace("/", "-");
+
+        return url;
+
     });
 
+    /*
     $(document).on('load', '#suma', function () {
 
         var num1 = Math.random;
         var num2 = Math.random;
         return num1 + num2;
-    });
+    });*/
 });
