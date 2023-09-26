642915dd05145adbe486435f2322c0beb1fdf272
Marco <marco.pertegal.prieto@gmail.com>
2023-09-26 20:41:36 +0200

    Acutalizacion prueba inicial

--- evaluacionInicial/index.js
+++ evaluacionInicial/index.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
         var title = $('#title').val();
         var description = $('#description').val();
         var url = $('#slug').val();
+        var robot = $('#robot-check').val();
 
         if (title == '') {
             $('#title').addClass('is-invalid');
@@ -17,15 +18,42 @@ $(document).ready(function () {
             $('#slug').addClass('is-invalid');
         }
 
+        if (robot == '' || checkSuma() == false) {
+            isValidForm = false;
+            $('#robot-check').addClass('is-invalid');
+        }
+
+        if (isValidForm == true) {
+            alert('Perfecto, vamos a publicar tu noticia!');
+        } else {
+            $('#robot-check').val('');
+            generateRandomSum();
+        }
+
     });
 
 
     $(document).on('click', '#createURL', function () {
-
         var url = $('#slug').val();
-
-        var url1 = url.toLowerCase().replace(' ', '-');//trimp es para quitar espacios en blanco delante y detras
-
-        $('#slug').val(url1);
+        urlArray = url.toLowerCase().split(' ');
+        result = urlArray.join("-");
+        $('#slug').val(result);
     });
+
+    function generateRandomSum() {
+        var n1 = between(0, 10);
+        var n2 = between(0, 10);
+        $('#n1').text(n1);
+        $('#n2').text(n2);
+    }
+
+    function checkSuma() {
+        return parseInt($('#robot-check').val()) == n1 + n2 ? true : false;
+    }
+
+    function between(min, max) {
+        return Math.floor(
+            Math.random() * (max - min) + min
+        )
+    }
 });
